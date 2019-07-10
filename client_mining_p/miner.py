7ec5dcd3873808e393908b877e828d82bb828176
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-10 17:55:30 -0400

    Removed proof_of_work from BlockChain class. Moved it, and copied valid_proof, into miner.py

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -6,6 +6,28 @@ import sys
 
 # TODO: Implement functionality to search for a proof 
 
+def proof_of_work(self, last_proof):
+    """
+    Simple Proof of Work Algorithm
+    - Find a number p' such that hash(pp') contains 4 leading
+    zeroes, where p is the previous p'
+    - p is the previous proof, and p' is the new proof
+    """
+
+    proof = 0
+    while self.valid_proof(last_proof, proof) is False:
+        proof += 1
+
+    return proof
+
+def valid_proof(last_proof, proof):
+    """
+    Validates the Proof:  Does hash(last_proof, proof) contain 4
+    leading zeroes?
+    """
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:1] == "0"
 
 if __name__ == '__main__':
     # What node are we interacting with?
