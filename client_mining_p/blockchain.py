fc299006b0d36bb63966e7803a6dfe11a686ffec
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-10 17:42:56 -0400

    Modified the mine endpoint to receive and validate a proof

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -140,32 +140,43 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
+@app.route('/mine', methods=['POST'])
 def mine():
     # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
+    values = request.get_json()
+
+    if 'proof' not in values:
+        return 'Missing Values', 401
+    
+    # last_block = blockchain.last_block
+    proof = values['proof']
+    last_proof = blockchain.last_block['proof']
+    valid_proof = blockchain.valid_proof(last_proof, proof)
+
+    if valid_proof:
+        return 'Validated', 200
+    else:
+        return 'Invalid proof', 400
 
     # We must receive a reward for finding the proof.
     # The sender is "0" to signify that this node has mine a new coin
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
+    # blockchain.new_transaction(
+    #     sender="0",
+    #     recipient=node_identifier,
+    #     amount=1,
+    # )
 
-    # Forge the new BLock by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
+    # # Forge the new BLock by adding it to the chain
+    # previous_hash = blockchain.hash(last_block)
+    # block = blockchain.new_block(proof, previous_hash)
 
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
+    # response = {
+    #     'message': "New Block Forged",
+    #     'index': block['index'],
+    #     'transactions': block['transactions'],
+    #     'proof': block['proof'],
+    #     'previous_hash': block['previous_hash'],
+    # }
     return jsonify(response), 200
 
 
@@ -198,7 +209,7 @@ def full_chain():
 @app.route('/last_proof', methods=['GET'])
 def last_proof():
     response = {
-        'last_proof': blockchain.chain[-1]["proof"]
+        'last_proof': blockchain.last_block["proof"]
     }
     return jsonify(response), 200
 
